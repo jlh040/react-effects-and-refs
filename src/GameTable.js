7dbe3838afc8c67e5dc99e62e1da78bc1127e013
Jeff Hall <jeff.leon.hall@gmail.com>
2021-08-05 11:19:39 -0700

    updated the code so that now the deck is shuffled, and added another useEffect for drawing cards

--- src/GameTable.js
+++ src/GameTable.js
@@ -4,22 +4,41 @@ import axios from 'axios';
 
 const GameTable = () => {
   const [cards, setCards] = useState([]);
+  const [isDrawing, setIsDrawing] = useState(false);
   const deckId = useRef();
-  const handleChange = async () => {
-    const res = await axios.get(`http://deckofcardsapi.com/api/deck/${deckId.current}/draw/?count=1`);
-    const {code, image} = res.data.cards[0];
-    setCards(cardArr => [
-      ...cardArr,
-      {code, image}
-    ])
+  const intervalId = useRef()
+  const handleChange = () => {
+    isDrawing ? clearInterval(intervalId) && setIsDrawing(false) : setIsDrawing(true);
   };
-  
+
   useEffect(() => {
     (async () => {
       const res = await axios.get('http://deckofcardsapi.com/api/deck/new/');
       deckId.current = res.data.deck_id;
+      await axios.get(`http://deckofcardsapi.com/api/deck/${deckId.current}/shuffle/`)
+
     })();
-  }, [])
+  }, []);
+
+  useEffect(() => {
+    if (isDrawing) {
+      intervalId.current = setInterval(async () => {
+        const res = await axios.get(`http://deckofcardsapi.com/api/deck/${deckId.current}/draw/?count=1`);
+      
+        if (res.data.error) {
+          alert('Error: no cards remaining!');
+          clearInterval(intervalId);
+          return;
+        }
+  
+        const {code, image} = res.data.cards[0];
+        setCards(cardArr => [
+          ...cardArr,
+          {code, image}
+        ])
+        }, 1000)
+    }
+  }, [isDrawing])
 
 
   return (
